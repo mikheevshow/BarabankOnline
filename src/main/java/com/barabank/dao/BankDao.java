bf6a5f520818f010b26e6cf6a6eae996f31955ed
ilyamikheev <mikheev.show@gmail.com>
2018-09-14 14:29:43 +0300

    Рефакторинг кода

--- src/main/java/com/barabank/dao/BankDao.java
+++ src/main/java/com/barabank/dao/BankDao.java
@@ -1,8 +1,10 @@
 package com.barabank.dao;
 
 import com.barabank.beans.*;
+import com.barabank.service.logic.TransactionReportType;
 
 import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -43,5 +45,13 @@ public interface BankDao {
     public List<Transaction> findAllSentAccountTransactions(long account_id);
     public List<Transaction> findAllReceivedAccountTransactions(long account_id);
     public void saveTransaction(Transaction transaction);
+
+
+    public List<Transaction> getBankTransactionsForPeriod(LocalDate startDate, LocalDate endDate);
+    public List<Transaction> getBankTransactionsForDay(LocalDate date);
+    public List<Transaction> getTransactionsForCustomerInPeriod(Customer customer, LocalDate startDate, LocalDate endDate);
+    public List<Transaction> getTransactionsForCustomerInDate(Customer customer, LocalDate date);
+    public List<Transaction> getTransactionsForAccountInPeriod(long account, LocalDate startDate, LocalDate endDate);
+    public List<Transaction> getTransactionsForAccountInDate(long account, LocalDate date);
 
 }
