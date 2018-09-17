001c6c1e15c8cdfc3c367efb9201424ce0283f88
ilyamikheev <mikheev.show@gmail.com>
2018-09-17 20:12:13 +0300

    Мелкие правки Добавлены страницы для пересылки средств

--- src/main/webapp/WEB-INF/resources/js/accounts_list.js
+++ src/main/webapp/WEB-INF/resources/js/accounts_list.js
@@ -1,14 +1,9 @@
-let dropdown = $('#accounts-list');
-
-dropdown.empty();
-
-dropdown.append('<option selected="true" disabled>Выберете счёт</option>');
-dropdown.prop('selectedIndex', 0);
-
-const url = 'localhost:8016/money-transactions/banktransaction/accounts';
-
-$.getJSON(url, function (data) {
-    $.each(data, function (key, entry) {
-        dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
-    })
+jQuery().ready(function () {
+    var tabAccounts = [];
+    $.getJSON('localhost:8080/money-transactions/banktransaction/accounts', function(data) {
+        $.each(data, function (index, val) {
+            tabAccounts[index] = val;
+        });
+        console.log(tabAccounts);
+    });
 });
