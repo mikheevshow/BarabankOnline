8525a24988c396ca517696de55f4717dc0ca303f
top4ikru <zmaster-leo@yandex.ru>
2018-09-23 23:41:49 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/barabank/dao/BankDao.java
    #	src/main/java/com/barabank/dao/BarabankDao.java
    #	src/main/java/com/barabank/dao/hello.java
    #	src/main/java/com/barabank/service/logic/BarabankCardOperationService.java
    #	src/main/java/com/barabank/service/logic/BarabankTransactionReportService.java
    #	src/main/java/com/barabank/service/logic/BarabankTransactionService.java
    
    GOTOVO =)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,58 @@
+CREATE TABLE public.customer
+(
+    phone bigint NOT NULL,
+    password varchar,
+    customer_id bigint PRIMARY KEY NOT NULL
+);
+CREATE UNIQUE INDEX customer_phone_uindex ON public.customer (phone);
+CREATE UNIQUE INDEX customer_customer_id_uindex ON public.customer (customer_id);
+
+
 CREATE TABLE public.person
 (
-    id bigint PRIMARY KEY NOT NULL,
+    passport_id bigint PRIMARY KEY NOT NULL,
     first_name varchar NOT NULL,
     last_name varchar NOT NULL,
     middle_name varchar NOT NULL,
-    phone bigint NOT NULL,
+    customer_id bigint NOT NULL,
     address varchar,
-    column_7 int,
-    birth_data timestamp NOT NULL
+    birth_date varchar NOT NULL,
+    CONSTRAINT person_customer_customer_id_fk FOREIGN KEY (customer_id) REFERENCES public.customer (customer_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE UNIQUE INDEX person_phone_uindex ON public.person (customer_id);
-CREATE UNIQUE INDEX person_id_uindex ON public.person (id);
 
-
-CREATE TABLE public.customer
+CREATE TABLE public.account
 (
-    phone bigint PRIMARY KEY NOT NULL,
-    password varchar NOT NULL
+    account_id bigint PRIMARY KEY NOT NULL,
+    balance numeric NOT NULL,
+    customer_id bigint NOT NULL,
+    CONSTRAINT account_customer_customer_id_fk FOREIGN KEY (customer_id) REFERENCES public.customer (customer_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE UNIQUE INDEX customer_phone_uindex ON public.customer (phone);
+
+CREATE TABLE public.transaction
+(
+    transaction_id bigint PRIMARY KEY NOT NULL,
+    from_account bigint NOT NULL,
+    to_account bigint NOT NULL,
+    sum numeric NOT NULL,
+    date timestamp
+);
+
+CREATE sequence account_id_seq
+    minvalue 1
+    start with 1
+    increment by 20;
+
+CREATE sequence customer_id_seq
+    minvalue 1
+    start with 1
+    increment by 20;
+
+CREATE sequence transaction_id_seq
+    minvalue 1
+    start with 1
+    increment by 20;
+
+CREATE sequence bankCard_id_seq
+    minvalue 1
+    start with 1
+    increment by 20;
